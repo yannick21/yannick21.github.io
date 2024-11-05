fe65600a0ad8a805ef9304a2401efe8bbc76d7bb
yannick <62220830+yannick21@users.noreply.github.com>
2024-11-05 16:14:07 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["SIEM", "SOAR", "SOC", "Red Teaming", "Incident Response"],
+    strings: ["SIEM/SOAR/SOC", "Red Teaming", "Incident Response"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
